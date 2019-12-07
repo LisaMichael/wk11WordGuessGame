0c2e7e0fb007d82963dde1a901bbb89c24013002
Lisa <lisa.michael@hotmail.com>
2019-12-07 13:44:16 -0500

    most recent letter.js

--- letter.js
+++ letter.js
@@ -7,7 +7,6 @@ var Letter = function(isInWord=false, guessLetter) {
    // this.isInWord=isInWord || false;
 
    
-    
   
 
 // A string value to store the underlying character for the letter
@@ -21,11 +20,24 @@ this.isInWord = isInWord
 
 // A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
 
+this.toString = function () {
 
+  if (this.guessLetter === " ") {
+      this.isInWord = true;
+      return " ";
+  } else {
+      if (this.isInWord === false) {
+          return "_";
+      } else {
+          return this.guessLetter;
+      }
+  }
 
 // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
 
+function() {
 
+}
 
 };
 
